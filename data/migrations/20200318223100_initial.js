d3ea5584c67eb75298cace7037c39c8afef6cc21
Michael Williams <manicdesigner@gmail.com>
2020-03-18 22:52:17 -0400

    built out a config for a knex config migration and seed data for testing TDD

---
+++ data/migrations/20200318223100_initial.js
@@ -0,0 +1,17 @@
+/** @format */
+
+exports.up = async function(knex) {
+  await knex.schema.createTable('answers', tbl => {
+    tbl.increments('id');
+    tbl
+      .string('title')
+      .notNullable()
+      .unique();
+    tbl.text('solution').notNullable();
+    tbl.text('comments').nullable();
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('answers');
+};
