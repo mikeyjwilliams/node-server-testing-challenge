badacb922da240c308526bc830760846103c7549
Michael Williams <manicdesigner@gmail.com>
2020-03-19 22:06:17 -0400

    answer-router w/ spec

--- answers/answers-router.js
+++ answers/answers-router.js
@@ -4,4 +4,41 @@ const express = require('express');
 const AnsModel = require('./answers-model');
 const router = express.Router();
 
+router.post('/', async (req, res, next) => {
+  const { title, solution, comment } = req.body;
+  if (!title) {
+    return res.status(400).json({ message: 'title is required' });
+  }
+  if (!solution) {
+    return res.status(400).json({ message: 'solution is required' });
+  }
+  const answer = {
+    title: title,
+    solution: solution,
+    comment: comment,
+  };
+  try {
+    const answers = await AnsModel.add(answer);
+    res.status(201).json(answers);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const gone = await AnsModel.remove(id);
+
+    if (gone) {
+      res.status(204).end();
+    } else {
+      res.status(400).json({ message: 'answer ID not found' });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
--- answers/answers-router.spec.js
+++ answers/answers-router.spec.js
@@ -1,14 +1,61 @@
 /** @format */
 
+const supertest = require('supertest');
 const db = require('../data/config');
 const answerModel = require('./answers-model');
+const server = require('../server');
 
 beforeEach(async () => {
   await db.seed.run();
 });
 
-// describe('get all answers route', () => {
-//     test('GET => answers', async () => {
-//         const res = await answerModel.
-//     })
-// })
+describe('create resource', () => {
+  it('POST /answers fail -> title required', async () => {
+    const res = await supertest(server)
+      .post('/answers')
+      .send({ title: null });
+
+    expect(res.statusCode).toBe(400);
+    expect(res.type).toBe('application/json');
+    expect(res.body.message).toBe('title is required');
+  });
+
+  it('POST /answers fail => solution required', async () => {
+    const res = await supertest(server)
+      .post('/answers')
+      .send({ title: 'jest solution', solution: null });
+
+    expect(res.statusCode).toBe(400);
+    expect(res.type).toBe('application/json');
+    expect(res.body.message).toBe('solution is required');
+  });
+
+  it('POST /answer pass all data', async () => {
+    const answer = {
+      title: 'node.js solution',
+      solution: 'check the docs',
+      comment: 'the docs are full of helpful advice',
+    };
+    const res = await supertest(server)
+      .post('/answers')
+      .send(answer);
+    expect(res.statusCode).toBe(201);
+    expect(res.type).toBe('application/json');
+    expect(res.body.title).toMatch(/node.js solution/i);
+  });
+});
+
+describe('delete resource', () => {
+  it('delete /answers/:id fails', async () => {
+    const res = await supertest(server).delete('/answers/6');
+
+    expect(res.statusCode).toBe(400);
+    expect(res.type).toBe('application/json');
+    expect(res.body.message).toMatch(/answer id not found/i);
+  });
+
+  it('delete /answers/4 passes', async () => {
+    const res = await supertest(server).delete('/answers/3');
+    expect(res.statusCode).toBe(204);
+  });
+});
